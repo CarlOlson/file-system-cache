e40f8a8996c2549a8a78693aeb6e877bec877cd9
CarlOlson <CarlOlson@users.noreply.github.com>
2026-05-15 15:19:55 +0900

    use mock timers in tests for performance

--- src/test/cache.expires.TEST.ts
+++ src/test/cache.expires.TEST.ts
@@ -1,45 +1,59 @@
 import assert from 'node:assert/strict';
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import { FileSystemCache } from '../FileSystemCache.ts';
-import { Sleep } from './common.ts';
 
 describe('expires', () => {
   it('cache does NOT expire (various types)', async () => {
-    using cache1 = FileSystemCache.disposable({ ttl: 10 });
-    const cache2 = new FileSystemCache({ basePath: cache1.basePath, ttl: 10 });
+    mock.timers.enable({ apis: ['Date'] });
+    try {
+      using cache1 = FileSystemCache.disposable({ ttl: 10 });
+      const cache2 = new FileSystemCache({ basePath: cache1.basePath, ttl: 10 });
 
-    await cache1.set('foo-1', 'bar-1');
-    await cache1.set('foo-2', 'bar-2', 0);
-    await cache1.set('foo-3', 'bar-3', 10);
+      await cache1.set('foo-1', 'bar-1');
+      await cache1.set('foo-2', 'bar-2', 0);
+      await cache1.set('foo-3', 'bar-3', 10);
 
-    assert.equal(await cache2.get('foo-1'), 'bar-1');
-    await Sleep.secs(1);
+      assert.equal(await cache2.get('foo-1'), 'bar-1');
+      mock.timers.tick(1000);
 
-    assert.equal(await cache2.get('foo-2'), 'bar-2');
-    assert.equal(await cache2.get('foo-3'), 'bar-3');
+      assert.equal(await cache2.get('foo-2'), 'bar-2');
+      assert.equal(await cache2.get('foo-3'), 'bar-3');
+    } finally {
+      mock.timers.reset();
+    }
   });
 
   it('cache DOES expires (various types)', async () => {
-    using cache1 = FileSystemCache.disposable({ ttl: 0.3 });
-    const cache2 = new FileSystemCache({ basePath: cache1.basePath, ttl: 0.3 });
+    mock.timers.enable({ apis: ['Date'] });
+    try {
+      using cache1 = FileSystemCache.disposable({ ttl: 0.3 });
+      const cache2 = new FileSystemCache({ basePath: cache1.basePath, ttl: 0.3 });
 
-    await cache1.set('number', 123);
-    await cache1.set('object', { foo: 456 }, 1);
+      await cache1.set('number', 123);
+      await cache1.set('object', { foo: 456 }, 1);
 
-    await Sleep.secs(1);
-    assert.equal(await cache2.get('number'), undefined);
-    assert.equal(await cache2.get('object'), undefined);
+      mock.timers.tick(1100);
+      assert.equal(await cache2.get('number'), undefined);
+      assert.equal(await cache2.get('object'), undefined);
+    } finally {
+      mock.timers.reset();
+    }
   });
 
   it('after expiring empty value is returned', async () => {
-    using cache = FileSystemCache.disposable({ ttl: 0.3 });
-    await cache.set('my-number', 123);
+    mock.timers.enable({ apis: ['Date'] });
+    try {
+      using cache = FileSystemCache.disposable({ ttl: 0.3 });
+      await cache.set('my-number', 123);
 
-    const res1 = await cache.getSync('my-number');
-    await Sleep.secs(0.4);
+      const res1 = cache.getSync('my-number');
+      mock.timers.tick(400);
 
-    const res2 = await cache.getSync('my-number');
-    assert.equal(res1, 123);
-    assert.equal(res2, undefined);
+      const res2 = cache.getSync('my-number');
+      assert.equal(res1, 123);
+      assert.equal(res2, undefined);
+    } finally {
+      mock.timers.reset();
+    }
   });
 });
--- src/test/common.ts
+++
@@ -1,4 +0,0 @@
-export const Sleep = {
-  msecs: (delay: number) => new Promise<void>((resolve) => setTimeout(resolve, delay)),
-  secs: (delay: number) => Sleep.msecs(delay * 1000),
-} as const;
